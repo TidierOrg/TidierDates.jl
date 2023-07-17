b6c91ad93bbd019c3bbe008f73b961e73840ace2
drizk1 <rizkytennis@gmail.com>
2023-07-17 10:42:27 -0400

    remove runtest.jl text

--- test/runtests.jl
+++
@@ -1,8 +0,0 @@
-using TidierDates
-using Test
-
-@testset "TidierDates.jl" begin
-    println(mdy("12/03/2020"))  # Output: 2020-12-03
-    println(mdy("12-03-2020"))  # Output: 2020-12-03
-    println(mdy(missing))  # Output: missing
-end
