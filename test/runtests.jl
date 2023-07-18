a9b94800dde04da13f7f5d53aec16f009c8f7d29
drizk1 <rizkytennis@gmail.com>
2023-07-17 22:50:39 -0400

    passed doctest, fixed diftime

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,11 @@
+module TestTidierDates
+
 using TidierDates
 using Test
+using Documenter
 
-@testset "TidierDates.jl" begin
-    println(mdy("12/03/2020"))  # Output: 2020-12-03
-    println(mdy("12-03-2020"))  # Output: 2020-12-03
-    println(mdy(missing))  # Output: missing
+DocMeta.setdocmeta!(TidierDates, :DocTestSetup, :(using TidierDates); recursive=true)
+
+doctest(TidierDates)
+
 end
