c379d2d7cff29b05a08ccc9a4f64f6de347b5a1a
webofceco <mapleselijah9@gmail.com>
2024-06-08 20:14:21 -0400

    pm and am fix

--- src/datedocstrings.jl
+++ src/datedocstrings.jl
@@ -323,10 +323,10 @@ A boolean indicating whether the time is in the morning.
 # Examples
 ```jldoctest
 julia> am(DateTime(2023, 6, 15, 9, 30, 0))
-false
+true
 
 julia> am(DateTime(2023, 6, 15, 8, 30, 0))
-false
+true
 ```
 """
 
@@ -345,10 +345,10 @@ A boolean indicating whether the time is in the afternoon.
 # Examples
 ```jldoctest
 julia> pm(DateTime(2023, 6, 15, 9, 30, 0))
-true
+false
 
 julia> pm(DateTime(2023, 6, 15, 8, 30, 0))
-true
+false
 ```
 """
 
